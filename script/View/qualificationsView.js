a1302e226864502cf22da268a49a219c1b96d9b2
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-02-15 23:17:30 +0400

    cascade towers

--- script/View/qualificationsView.js
+++ script/View/qualificationsView.js
@@ -1,11 +1,12 @@
 class QualificationsView {
   introBox = document.querySelector('.intro-part');
   introText = document.querySelector('.intro-part__text-box');
-  kahnInvisible = document.querySelector('.kahn-part__invisible');
+  // kahnInvisible = document.querySelector('.kahn-part__invisible');
   choiceBox = document.querySelector('.choice-part');
-  kahnBox = document.querySelector('.kahn-part');
+  // kahnBox = document.querySelector('.kahn-part');
   towerBox = document.querySelector('.choice-part__towers');
   bothTowers = document.querySelectorAll('.towers');
+  towerDescription = document.querySelectorAll('.tower-description');
   leftTower = document.querySelector('.tower-short');
   rightTower = document.querySelector('.tower-long');
   imgBoxesShort = document.querySelectorAll('.img-box__short');
@@ -29,13 +30,24 @@ class QualificationsView {
   // });
 
   qualsChosen;
+  transformConditions;
 
   constructor() {
     // this.kahnObserver.observe(this.kahnInvisible);
-    //prettier-ignore
-    this.bothTowers.forEach((tower) => tower.addEventListener('click', function (e) {
+    this.positionImages();
+    // prettier-ignore
+    this.bothTowers.forEach((tower) => {
+      tower.addEventListener('click', function (e) {
         this.chooseTower(e);
-    }.bind(this)));
+      }.bind(this));
+    tower.addEventListener('mouseover', function (e) {
+      this.imageStandOut(e);
+      }.bind(this));
+      tower.addEventListener('mouseout', function (e) {
+        this.returnImageTransformValues(e);
+        }.bind(this));
+      
+    });
     //prettier-ignore
     this.btnCloseQuals.addEventListener('click', function () {
         this.closeDisplayQualifications(this.qualsChosen);
@@ -54,6 +66,63 @@ class QualificationsView {
     });
   }
 
+  positionImages(width = window.innerWidth) {
+    console.log(width < 1080);
+    this.imgBoxesShort.forEach((box) => {
+      console.log(+box.dataset.pos);
+      this.imageStyleRender(box);
+      // box.style.transform = `translateZ(-${box.dataset.pos}rem) translateY(${+box.dataset.pos / 1.5}rem) translateX(${
+      //   box.dataset.pos / 2
+      // }rem)`;
+      // box.style.opacity = 1 - +box.dataset.pos / 10;
+    });
+    this.imgBoxesLong.forEach((box) => {
+      console.log(+box.dataset.pos);
+      this.imageStyleRender(box);
+      // box.style.transform = `translateZ(-${box.dataset.pos}rem) translateY(${+box.dataset.pos / 1.5}rem) translateX(${
+      //   box.dataset.pos / 2
+      // }rem)`;
+      // box.style.opacity = 1 - +box.dataset.pos / 10;
+    });
+    // if (width > 640) {
+    //   this.imgBoxesShort.forEach((box) => {
+    //     console.log(+box.dataset.pos);
+    //     box.style.transform = `translateZ(${box.dataset.pos}rem) translateY(${+box.dataset.pos / 8}rem) translateX(${
+    //       +box.dataset.pos / 1
+    //     }rem)`;
+    //     box.style.opacity = 1 - (1 - +box.dataset.pos / 10) + 0.4;
+    //   });
+    //   this.imgBoxesLong.forEach((box) => {
+    //     console.log(+box.dataset.pos);
+    //     box.style.transform = `translateZ(${box.dataset.pos}rem) translateY(${+box.dataset.pos / 8}rem) translateX(${
+    //       +box.dataset.pos / 1
+    //     }rem)`;
+    //     box.style.opacity = 1 - (1 - +box.dataset.pos / 10) + 0.1;
+    //   });
+    // }
+  }
+
+  imageStandOut(e) {
+    if (!e.target.closest('.img-box')) return;
+    const element = e.target.closest('.img-box');
+    element.style.transform = `translateZ(10rem) translateY(${+element.dataset.pos / 1.5}rem) translateX(${
+      element.dataset.pos / 2
+    }rem)`;
+    element.style.opacity = `1`;
+  }
+
+  returnImageTransformValues(e) {
+    if (!e.target.closest('.img-box')) return;
+    this.imageStyleRender(e.target.closest('.img-box'));
+  }
+
+  imageStyleRender(element) {
+    element.style.transform = `translateZ(-${element.dataset.pos}rem) translateY(${
+      +element.dataset.pos / 1.5
+    }rem) translateX(${element.dataset.pos / 2}rem)`;
+    element.style.opacity = `${1 - +`${element.dataset.pos >= 10 ? 9.5 : element.dataset.pos}` / 10 + 0.05}`;
+  }
+
   kahnAttached(entry, observer) {
     console.log(entry[0]);
     if (!entry[0].isIntersecting && entry[0].boundingClientRect.top <= 0) {
@@ -64,34 +133,22 @@ class QualificationsView {
 
   chooseTower(e) {
     this.choiceBox.scrollIntoView({ block: 'start', behavior: 'smooth' });
-    this.towersBase.style.visibility = 'hidden';
     if (e.target.closest('.tower-short')) {
       this.qualsChosen = 'short';
       this.rightTower.style.opacity = '0';
       this.rightTower.style.visibility = 'hidden';
-      this.imgBoxesLong.forEach((box) => (box.style.visibility = 'hidden'));
-      // this.leftTower.style.transform = 'translateX(50%)';
-      window.innerWidth <= 1079
-        ? (this.leftTower.style.transform = 'translateX(25vw)')
-        : (this.leftTower.style.transform = 'translateY(18vh)');
-      //prettier-ignore
-      setTimeout(function () {this.leftTower.style.visibility = 'hidden';}.bind(this),900);
-      this.imgBoxesShort.forEach((box) => {
-        //prettier-ignore
-        setTimeout(function () {box.style.transform = 'scale(6)';}.bind(this),1200);
-        //prettier-ignore
-        setTimeout(function () {box.style.opacity = '0';}.bind(this),1200);
-        //prettier-ignore
-        setTimeout(function () {box.style.visibility = 'hidden';}.bind(this),1400);
-      });
+      window.innerWidth <= 639
+        ? (this.leftTower.style.transform = 'translateY(25vh)')
+        : (this.leftTower.style.transform = 'translateY(25vh) translateX(-25vw)');
       setTimeout(
         function () {
-          this.towerBox.style.display = 'none';
+          this.leftTower.style.opacity = '0';
+        }.bind(this),
+        1200
+      );
+      setTimeout(
+        function () {
           this.openDisplayQualifications('short');
-          this.choiceBox.classList.add('choice-background');
-          this.introBox.classList.add('choice-background');
-          this._makeElementDisappear(this.kahnBox, 500);
-          this._makeElementAppear(this.btnCloseQuals, 500, 'inline-block');
           this._makeElementAppear(this.eduIT, 500, 'inline-block');
         }.bind(this),
         1800
@@ -101,42 +158,28 @@ class QualificationsView {
       this.qualsChosen = 'long';
       this.leftTower.style.opacity = '0';
       this.leftTower.style.visibility = 'hidden';
-      this.imgBoxesShort.forEach((box) => (box.style.visibility = 'hidden'));
-      // this.rightTower.style.transform = 'translateX(-50%)';
-      window.innerWidth <= 1079
-        ? (this.rightTower.style.transform = 'translateX(-25vw)')
-        : (this.rightTower.style.transform = 'translateY(-18vh)');
+      window.innerWidth <= 639
+        ? (this.rightTower.style.transform = 'translateY(-25vh)')
+        : (this.rightTower.style.transform = 'translateY(-18vh) translateX(22vw)');
       //prettier-ignore
-      setTimeout(function () {this.rightTower.style.visibility = 'hidden';}.bind(this),900);
-      this.imgBoxesLong.forEach((box) => {
-        //prettier-ignore
-        setTimeout(function () {box.style.transform = 'scale(6)';}.bind(this),1200);
-        //prettier-ignore
-        setTimeout(function () {box.style.opacity = '0';}.bind(this),1200);
-        //prettier-ignore
-        setTimeout(function () {box.style.visibility = 'hidden';}.bind(this),1400);
-      });
+      setTimeout(function () {this.rightTower.style.opacity = '0';}.bind(this),1200);
       setTimeout(
         function () {
-          this.towerBox.style.display = 'none';
           this.openDisplayQualifications('long');
-          this.choiceBox.classList.add('choice-background');
-          this.introBox.classList.add('choice-background');
-          this._makeElementDisappear(this.kahnBox, 500);
-          this._makeElementAppear(this.btnCloseQuals, 500, 'inline-block');
           this._makeElementAppear(this.eduAll, 500, 'inline-block');
         }.bind(this),
         1800
       );
     }
+    this.towerDescription.forEach((desc) => {
+      this._makeElementDisappear(desc, 1700);
+    });
     setTimeout(() => {
+      this._makeElementAppear(this.btnCloseQuals, 500, 'inline-block');
+      this.towerBox.style.display = 'none';
       this.bothTowers.forEach((tower) => {
-        tower.style.transform = 'translateX(0)';
+        tower.style.transform = 'translateX(0) translateY(0)';
       });
-      this.allImageBoxes.forEach((box) => {
-        box.style.transform = 'scale(1)';
-      });
-      this.choiceBox.style.marginTop = '1rem';
     }, 1800);
   }
 
@@ -144,21 +187,15 @@ class QualificationsView {
     setTimeout(
       function () {
         this.towerBox.style.display = 'flex';
-        this.allImageBoxes.forEach((box) => {
-          box.style.visibility = 'visible';
-          setTimeout(function () {
-            box.style.opacity = '1';
-          }, 50);
-        });
         this.bothTowers.forEach((tower) => {
           tower.style.visibility = 'visible';
           setTimeout(function () {
             tower.style.opacity = '1';
-          }, 50);
+          }, 100);
         });
-        this.towersBase.style.visibility = 'visible';
-        this.choiceBox.classList.remove('choice-background');
-        this.introBox.classList.remove('choice-background');
+        this.towerDescription.forEach((desc) => {
+          this._makeElementAppear(desc, 500, 'block');
+        });
       }.bind(this),
       550
     );
@@ -186,7 +223,7 @@ class QualificationsView {
       this._makeElementDisappear(this.eduAll, 500);
     }
     this._makeElementDisappear(this.btnCloseQuals, 500);
-    this._makeElementAppear(this.kahnBox, 1000, 'flex');
+    // this._makeElementAppear(this.kahnBox, 1000, 'flex');
   }
 
   _makeElementAppear(element, timer, display) {
