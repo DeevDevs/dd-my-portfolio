1f43cec2ff6726edf442dd25b67baecda68d71a1
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-02-23 19:59:48 +0400

    wheel anim done

--- script/View/projectsView.js
+++ script/View/projectsView.js
@@ -7,9 +7,12 @@ class ProjectsView {
   wheelContainer = document.querySelector('.pr-main-section');
   draggedOverDiv = document.querySelector('.draggedover-div');
   draggedDiv = document.querySelector('.dragged-div');
-  log;
+
+  mousePositionX;
   rotationValue = 0;
   dragX = 0;
+  movingStore;
+  isMoving = true;
   dragDirection;
 
   // qualSectionObserver = new IntersectionObserver(this.cardLikeImages.bind(this), { root: null, threshold: 0.1 });
@@ -17,81 +20,129 @@ class ProjectsView {
   constructor() {
     this.positionDivs();
     this.introBox.addEventListener('mousemove', this.addIntroMovingShadow.bind(this));
-    //   this.qualSectionObserver.observe(this.choiceBox);
-    window.addEventListener('keydown', this.initiateSpin.bind(this));
+    // window.addEventListener('keydown', this.initiateSpin.bind(this));
     this.draggedOverDiv.addEventListener('mousemove', this.showMousePosition.bind(this));
+    this.draggedDiv.addEventListener('click', this.whereClicks.bind(this));
+    this.draggedDiv.addEventListener('dragstart', this.setGrabbingCursor.bind(this));
     this.draggedDiv.addEventListener('drag', this.seeDrag.bind(this));
-    this.draggedDiv.addEventListener('dragend', this.slowDrag.bind(this));
-    // this.draggedDiv.onscroll = (e) => console.log(e);
-    console.log(this.draggedDiv.getBoundingClientRect());
+    this.draggedDiv.addEventListener('dragend', this.endRotation.bind(this));
   }
 
-  ///////////// TESTING //////////////////
-  initiateSpin(e) {
-    if (e.key !== 'ArrowRight') {
-      if (e.key !== 'ArrowLeft') return;
-    }
-    if (e.key === 'ArrowRight') {
-      this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
-      this.rotationValue++;
-    }
-    if (e.key === 'ArrowLeft') {
-      this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
-      this.rotationValue--;
-    }
-    console.log(this.rotationValue);
+  whereClicks() {
+    if (this.mousePositionX < window.innerWidth / 3) console.log('left');
+    if (this.mousePositionX > window.innerWidth / 3 && this.mousePositionX < (window.innerWidth * 2) / 3)
+      if (this.mousePositionX > (window.innerWidth * 2) / 3) console.log('right');
+  }
+
+  ///////////// WHEEL FUNCTIONS //////////////////
+  // initiateSpin(e) {
+  //   if (e.key !== 'ArrowRight') {
+  //     if (e.key !== 'ArrowLeft') return;
+  //   }
+  //   if (e.key === 'ArrowRight') {
+  //     this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
+  //     this.rotationValue++;
+  //   }
+  //   if (e.key === 'ArrowLeft') {
+  //     this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
+  //     this.rotationValue--;
+  //   }
+  // }
+
+  setGrabbingCursor(e) {
+    Draggable.create(this.draggedDiv, {
+      throwProps: true,
+      bounds: window,
+      edgeResistance: 0.7,
+      cursor: 'grab' /* set initial cursor on hover */,
+      onDragStart: function () {
+        TweenLite.set(this.draggedDiv, { cursor: 'grabbing' });
+      },
+      onDragEnd: function () {
+        TweenLite.set(this.draggedDiv, { cursor: 'grab' });
+      },
+    });
   }
 
   showMousePosition(e) {
-    this.draggedDiv.style.left = `${e.pageX - 20}px`;
-    this.draggedDiv.style.top = `${e.pageY - 20}px`;
+    this.mousePositionX = e.pageX;
+    this.draggedDiv.style.left = `${e.pageX - 10}px`;
+    this.draggedDiv.style.top = `${e.pageY - 10}px`;
+  }
+
+  checkIfMoves(curMoveX, curMoveY) {
+    if (curMoveX === 0 && curMoveY === 0) return;
+    if (curMoveX !== this.movingStore) {
+      this.isMoving = true;
+    } else {
+      this.isMoving = false;
+    }
+    if (curMoveX !== 0) this.movingStore = curMoveX;
   }
 
   seeDrag(e) {
-    console.log(e);
+    this.checkIfMoves(e.clientX, e.clientY);
+    // console.log(e);
+    // this.draggedDiv.style.cursor = 'grabbing';
+    // this.draggedOverDiv.style.cursor = 'grabbing';
     if (e.clientX > this.dragX) {
       this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
-      this.rotationValue += 0.35;
+      this.rotationValue += 0.3;
       this.dragX = e.clientX;
       if (e.y !== 0) this.dragDirection = 'right';
     }
     if (e.clientX < this.dragX && e.y !== 0) {
       this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
-      this.rotationValue -= 0.35;
+      this.rotationValue -= 0.3;
       this.dragX = e.clientX;
       this.dragDirection = 'left';
     }
-    console.log(this.dragDirection);
   }
 
-  // continueRotation(direction) {
-  //   if (direction === 'right') {
-  //     for (let i = 0; i < 10; i++) {
-  //       setTimeout(function(){}, i*10)
-  //     }
-  //   }
-  // }
+  endRotation(e) {
+    const remaining = Math.abs(this.rotationValue % 45);
+    if (this.isMoving) {
+      if (this.dragDirection === 'right') {
+        let value = this.rotationValue > 0 ? 1 + (45 - remaining) / 45 : 1 + remaining / 45;
+        this.slowRotation('right', value);
+      }
+      if (this.dragDirection === 'left') {
+        let value = this.rotationValue > 0 ? 1 + remaining / 45 : 1 + (45 - remaining) / 45;
+        this.slowRotation('left', value);
+      }
+    }
+    if (!this.isMoving) {
+      if (remaining >= 22.5) {
+        let value = (45 - remaining) / 45;
+        this.rotationValue > 0 ? this.slowRotation('right', value) : this.slowRotation('left', value);
+      }
+      if (remaining < 22.5) {
+        let value = remaining / 45;
+        this.rotationValue > 0 ? this.slowRotation('left', value) : this.slowRotation('right', value);
+      }
+    }
+  }
 
-  slowDrag(e) {
-    if (this.dragDirection === 'right') {
-      for (let i = 0; i < 15; i++) {
+  slowRotation(direction, value) {
+    if (direction === 'right') {
+      for (let i = 0; i <= 45; i++) {
         setTimeout(
           function () {
             this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
-            this.rotationValue += 1.5;
+            this.rotationValue += value;
           }.bind(this),
-          i * 30
+          i * 15
         );
       }
     }
-    if (this.dragDirection === 'left') {
-      for (let i = 0; i < 15; i++) {
+    if (direction === 'left') {
+      for (let i = 0; i <= 45; i++) {
         setTimeout(
           function () {
             this.wheel.style.transform = `rotateY(${this.rotationValue}deg)`;
-            this.rotationValue -= 1.5;
+            this.rotationValue -= value;
           }.bind(this),
-          i * 30
+          i * 15
         );
       }
     }
@@ -99,7 +150,6 @@ class ProjectsView {
 
   positionDivs() {
     this.justDivs.forEach((div) => {
-      // console.log(window.innerWidth);
       div.style.transform = `rotateY(${div.dataset.place}deg) translateZ(${window.innerWidth / 2.5}px)`;
     });
   }
