94e7f3a76649c5504f8be385b612a5b7319fb5d8
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-03-04 18:04:51 +0400

    nomorewheel

--- script/View/projectsView.js
+++ script/View/projectsView.js
@@ -14,17 +14,19 @@ class ProjectsView {
   draggedOverDiv = document.querySelector('.draggedover-div');
   draggedDiv = document.querySelector('.dragged-div');
   btnSwitchFormat = document.querySelector('.btn__switch-format');
+
   projectViewFormat = 'wheel';
-  currentPageCondition;
+
   mousePositionX;
   mouseSpeed = 1;
   lastMousePosition;
-  rotationValue = 0;
+  shiftValue = 0;
   dragX = 0;
   movingStore;
   isMoving = true;
   dragDirection;
-  newGrab = false;
+  currentDivsPosition = 0;
+
   projectNames = [
     'Mapty: Workout Records',
     'Bankist: Home Page',
@@ -35,21 +37,14 @@ class ProjectsView {
     'Pig Game',
     'Portfolio Website',
   ];
-  windowWidth = window.innerWidth;
-
-  // qualSectionObserver = new IntersectionObserver(this.cardLikeImages.bind(this), { root: null, threshold: 0.1 });
 
   constructor() {
-    window.addEventListener('resize', this.followResizing.bind(this));
-    this.btnSwitchFormat.addEventListener(
-      'click',
-      function () {
-        this.toggleProjectViewFormat(this.projectViewFormat);
-      }.bind(this)
-    );
-    if (window.innerWidth >= 1200 && window.innerWidth <= 2400) {
-      this.positionDivs();
-    } else this.toggleProjectViewFormat();
+    // this.btnSwitchFormat.addEventListener(
+    //   'click',
+    //   function () {
+    //     this.toggleProjectViewFormat(this.projectViewFormat);
+    //   }.bind(this)
+    // );
     this.introBox.addEventListener('mousemove', this.addIntroMovingShadow.bind(this));
     this.draggedOverDiv.addEventListener('mousemove', this.showMousePosition.bind(this));
     this.draggedDiv.addEventListener('click', this.whereClicks.bind(this));
@@ -58,101 +53,90 @@ class ProjectsView {
     this.draggedDiv.addEventListener('dragend', this.endRotation.bind(this));
   }
 
-  followResizing(e) {
-    console.log(e);
-    // if (this.projectViewFormat === 'wheel') {
-    //   this.positionDivs();
-    // }
-    if (e.target.innerWidth >= 1080) {
-      this.btnSwitchFormat.style.display = 'block';
-    }
-    if (
-      (e.target.innerWidth <= 1080 && this.projectViewFormat === 'wheel') ||
-      (e.target.innerWidth >= 2080 && this.projectViewFormat === 'wheel')
-    ) {
-      this.toggleProjectViewFormat();
-      this.btnSwitchFormat.style.display = 'none';
-    }
-  }
+  // toggleProjectViewFormat() {
+  //   // this.windowWidth = window.innerWidth;
+  //   if (this.projectViewFormat === 'wheel') {
+  //     this.wheelGridBox.classList.add('nowheel');
+  //     this.wheelGridBox.style.transform = 'rotateY(0deg)';
+  //     this.justDivs.forEach((div, i) => {
+  //       div.style.transform = `rotateY(0deg) translateZ(0rem)`;
+  //       div.classList.add('nowheel__divs');
+  //       div.style.position = 'relative';
+  //     });
+  //     this.justDivsContentBoxes.forEach((div) => {
+  //       div.classList.add('nowheel__divs-content');
+  //     });
+  //     this.mainImageContainers.forEach((container) => container.classList.add('nowheel__main-img-container'));
+  //     this.secondaryImageContainers.forEach((container) => container.classList.add('nowheel__secondary-img-container'));
+  //     this.individualProjectName.forEach((nameBox) => (nameBox.style.display = 'block'));
+  //     this.mainProjectName.style.display = 'none';
+  //     this.draggedDiv.style.display = 'none';
+  //     this.draggedOverDiv.style.display = 'none';
+  //     this.projectViewFormat = 'flex';
+  //   } else if (this.projectViewFormat === 'flex') {
+  //     this.wheelGridBox.classList.remove('nowheel');
+  //     this.wheelGridBox.style.transform = `rotateY(${this.rotationValue}deg)`;
+  //     this.justDivs.forEach((div) => {
+  //       div.classList.remove('nowheel__divs');
+  //       div.style.position = 'absolute';
+  //     });
 
-  toggleProjectViewFormat() {
-    // this.windowWidth = window.innerWidth;
-    if (this.projectViewFormat === 'wheel') {
-      this.wheelGridBox.classList.add('nowheel');
-      this.wheelGridBox.style.transform = 'rotateY(0deg)';
-      this.justDivs.forEach((div, i) => {
-        div.style.transform = `rotateY(0deg) translateZ(0rem)`;
-        div.classList.add('nowheel__divs');
-        div.style.position = 'relative';
-      });
-      this.justDivsContentBoxes.forEach((div) => {
-        div.classList.add('nowheel__divs-content');
-      });
-      this.mainImageContainers.forEach((container) => container.classList.add('nowheel__main-img-container'));
-      this.secondaryImageContainers.forEach((container) => container.classList.add('nowheel__secondary-img-container'));
-      this.individualProjectName.forEach((nameBox) => (nameBox.style.display = 'block'));
-      this.mainProjectName.style.display = 'none';
-      this.draggedDiv.style.display = 'none';
-      this.draggedOverDiv.style.display = 'none';
-      this.projectViewFormat = 'flex';
-    } else if (this.projectViewFormat === 'flex') {
-      this.wheelGridBox.classList.remove('nowheel');
-      this.wheelGridBox.style.transform = `rotateY(${this.rotationValue}deg)`;
-      this.justDivs.forEach((div) => {
-        div.classList.remove('nowheel__divs');
-        div.style.position = 'absolute';
-        div.style.transform = `rotateY(${div.dataset.place}deg) translateZ(${window.innerWidth / 40}rem)`;
-      });
-      this.justDivsContentBoxes.forEach((div) => {
-        div.classList.remove('nowheel__divs-content');
-      });
-      this.mainImageContainers.forEach((container) => container.classList.remove('nowheel__main-img-container'));
-      this.secondaryImageContainers.forEach((container) =>
-        container.classList.remove('nowheel__secondary-img-container')
-      );
-      this.individualProjectName.forEach((nameBox) => (nameBox.style.display = 'none'));
-      this.mainProjectName.style.display = 'block';
-      this.draggedDiv.style.display = 'block';
-      this.draggedOverDiv.style.display = 'flex';
-      this.projectViewFormat = 'wheel';
-      // this.positionDivs();
-    }
-  }
+  //     this.justDivsContentBoxes.forEach((div) => {
+  //       div.classList.remove('nowheel__divs-content');
+  //     });
+  //     this.mainImageContainers.forEach((container) => container.classList.remove('nowheel__main-img-container'));
+  //     this.secondaryImageContainers.forEach((container) =>
+  //       container.classList.remove('nowheel__secondary-img-container')
+  //     );
+  //     this.individualProjectName.forEach((nameBox) => (nameBox.style.display = 'none'));
+  //     this.mainProjectName.style.display = 'block';
+  //     this.draggedDiv.style.display = 'block';
+  //     this.draggedOverDiv.style.display = 'flex';
+  //     this.projectViewFormat = 'wheel';
+  //     // this.positionDivs();
+  //   }
+  // }
 
   whereClicks() {
-    if (this.mousePositionX < window.innerWidth / 4) console.log('left');
+    if (this.mousePositionX < window.innerWidth / 4) this.shiftProjects('left');
     if (this.mousePositionX > window.innerWidth / 4 && this.mousePositionX < (window.innerWidth * 3) / 4)
       console.log('center');
-    if (this.mousePositionX > (window.innerWidth * 3) / 4) console.log('right');
+    if (this.mousePositionX > (window.innerWidth * 3) / 4) this.shiftProjects('right');
+  }
+
+  shiftProjects(direction) {
+    if (direction === 'right' && this.currentDivsPosition < 7) this.currentDivsPosition++;
+    if (direction === 'left' && this.currentDivsPosition > 0) this.currentDivsPosition--;
+    this.wheelGridBox.style.transform = `translateX(${this.currentDivsPosition * -50}vw)`;
   }
 
   ////////////////////// FRONT DIV ANIMATION FUNCTIONS ///////////////////////////
 
   identifyFrontDiv() {
-    switch (Math.abs(this.rotationValue % 360)) {
+    switch (this.shiftValue) {
       case 0:
         this.revealProjectDetails('1-mapty');
         break;
-      case 45:
-        this.rotationValue > 0 ? this.revealProjectDetails('8-portfolio') : this.revealProjectDetails('2-bankist');
+      case -50:
+        this.revealProjectDetails('2-bankist');
         break;
-      case 90:
-        this.rotationValue > 0 ? this.revealProjectDetails('7-piggame') : this.revealProjectDetails('3-bankapp');
+      case -100:
+        this.revealProjectDetails('3-bankapp');
         break;
-      case 135:
-        this.rotationValue > 0 ? this.revealProjectDetails('6-connect') : this.revealProjectDetails('4-natours');
+      case -150:
+        this.revealProjectDetails('4-natours');
         break;
-      case 180:
+      case -200:
         this.revealProjectDetails('5-forkify');
         break;
-      case 225:
-        this.rotationValue > 0 ? this.revealProjectDetails('4-natours') : this.revealProjectDetails('6-connect');
+      case -250:
+        this.revealProjectDetails('6-connect');
         break;
-      case 270:
-        this.rotationValue > 0 ? this.revealProjectDetails('3-bankapp') : this.revealProjectDetails('7-piggame');
+      case -300:
+        this.revealProjectDetails('7-piggame');
         break;
-      case 315:
-        this.rotationValue > 0 ? this.revealProjectDetails('2-bankist') : this.revealProjectDetails('8-portfolio');
+      case -350:
+        this.revealProjectDetails('8-portfolio');
         break;
       default:
         break;
@@ -164,22 +148,14 @@ class ProjectsView {
     const mainContainer = frontDiv
       .querySelector('.justadiv__content')
       .querySelector('.jad__content-main-img__container');
-    // const mainImage = frontDiv
-    //   .querySelector('.justadiv__content')
-    //   .querySelector('.jad__content-main-img__container')
-    //   .querySelector('.jad__content-main-img');
-    // mainContainer.style.transform = ' scale(1.02) translateZ(0.5rem)';
     mainContainer.style.transform = 'scale(1.02)';
     mainContainer.style.opacity = 1;
-    // mainImage.style.display = 'block';
     const secondaryContainers = frontDiv
       .querySelector('.justadiv__content')
       .querySelectorAll('.jad__content-secondary-img__container');
     secondaryContainers.forEach((container) => {
       container.style.opacity = 1;
       container.style.transform = 'scale(1.1)';
-      // container.style.transform = ' scale(1.1)';
-      container.style.backfaceVisibility = 'hidden';
     });
     this.displayFrontProjectName(projectId);
     this.displayFrontProjectDetails(projectId);
@@ -196,19 +172,6 @@ class ProjectsView {
     this.wheelDetails.forEach((element) => {
       element.style.opacity = 1;
     });
-  }
-
-  //////////////////// POSITION DIVS AT PAGELOAD ///////////////////
-
-  positionDivs() {
-    // if (window.innerWidth >= 1080 && window.matchMedia('(hover: hover)').matches)
-    //   this.justDivs.forEach((div) => {
-    //     div.style.transform = `rotateY(${div.dataset.place}deg) translateZ(${window.innerWidth / 40}rem)`;
-    //   });
-    if (window.matchMedia('(hover: hover)').matches)
-      this.justDivs.forEach((div) => {
-        div.style.transform = `rotateY(${div.dataset.place}deg) translateZ(${window.innerWidth / 40}rem)`;
-      });
   }
 
   ///////////// WHEEL FUNCTIONS //////////////////
@@ -245,61 +208,48 @@ class ProjectsView {
     if (e.clientY !== 0) this.lastMousePosition = e.clientX;
     if (e.clientX > this.dragX) {
       // this.rotationValue += 0.4;
-      this.rotationValue = this.rotationValue + 0.2 * this.mouseSpeed;
-      this.wheelGridBox.style.transform = `rotateY(${this.rotationValue}deg)`;
+      this.shiftValue = this.shiftValue + 0.2 * this.mouseSpeed;
+      this.wheelGridBox.style.transform = `translateX(${this.shiftValue}vw)`;
       this.dragX = e.clientX;
       if (e.y !== 0) this.dragDirection = 'right';
     }
     if (e.clientX < this.dragX && e.y !== 0) {
-      // this.rotationValue -= 0.4;
-      this.rotationValue = this.rotationValue - 0.2 * this.mouseSpeed;
-      this.wheelGridBox.style.transform = `rotateY(${this.rotationValue}deg)`;
+      this.shiftValue = this.shiftValue - 0.2 * this.mouseSpeed;
+      this.wheelGridBox.style.transform = `translateX(${this.shiftValue}vw)`;
       this.dragX = e.clientX;
       this.dragDirection = 'left';
     }
   }
 
   endRotation() {
-    const remaining = Math.abs(this.rotationValue % 45);
+    const remaining = Math.abs(this.shiftValue % 50);
     if (this.isMoving) {
-      const distanceLeft = 45 - remaining;
-      if (this.rotationValue > 0) {
-        this.dragDirection === 'right'
-          ? this.slowRotation(this.dragDirection, distanceLeft)
-          : this.slowRotation(this.dragDirection, remaining);
-      }
-      if (this.rotationValue < 0) {
-        this.dragDirection === 'right'
-          ? this.slowRotation(this.dragDirection, remaining)
-          : this.slowRotation(this.dragDirection, distanceLeft);
-      }
+      const distanceLeft = 50 - remaining;
+      if (this.dragDirection === 'right') this.slowRotation(this.dragDirection, remaining);
+      if (this.dragDirection === 'left') this.slowRotation(this.dragDirection, distanceLeft);
     }
     if (!this.isMoving) {
-      if (this.rotationValue > 0) {
-        remaining > 22.5 ? (this.rotationValue += 45 - remaining) : (this.rotationValue -= remaining);
-        this.setWheelFinalPosition();
-      }
-      if (this.rotationValue < 0) {
-        remaining > 22.5 ? (this.rotationValue -= 45 - remaining) : (this.rotationValue += remaining);
-        this.setWheelFinalPosition();
-      }
+      remaining > 25 ? (this.shiftValue -= 50 - remaining) : (this.shiftValue += remaining);
+      this.setWheelFinalPosition();
     }
   }
 
   slowRotation(direction, value) {
     if (direction === 'right') {
-      this.rotationValue += value;
+      this.shiftValue += value;
       this.setWheelFinalPosition();
     }
     if (direction === 'left') {
-      this.rotationValue -= value;
+      this.shiftValue -= value;
       this.setWheelFinalPosition();
     }
   }
 
   setWheelFinalPosition() {
+    if (this.shiftValue > 0) this.shiftValue = 0;
+    if (this.shiftValue < -350) this.shiftValue = -350;
     this.wheelGridBox.style.transition = `transform 0.6s cubic-bezier(0.24, 1.44, 0.44, 1.19)`;
-    this.wheelGridBox.style.transform = `rotateY(${this.rotationValue}deg)`;
+    this.wheelGridBox.style.transform = `translateX(${this.shiftValue}vw)`;
     this.timer = setTimeout(
       function () {
         this.wheelGridBox.style.transition = `transform 0s`;
