fa809781ac707f4c564373c40bd756ecbff41b7c
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-03-11 19:15:51 +0400

    finishedindiv

--- script/View/indivProjectView.js
+++ script/View/indivProjectView.js
@@ -1,20 +1,22 @@
 class IndividualView {
   headerBox = document.querySelector('.header-menu');
   footerBox = document.querySelector('.footer-menu');
+  indivScrolledSection = document.querySelector('.indiv-section__scrolled-div');
   indivMainSection = document.querySelector('.indiv-section__main');
   indivContentSection = document.querySelector('.indiv-section__content');
   allIndivSections = document.querySelectorAll('.indiv-section-box');
 
+  maxWidthPossible;
   //section 1
   scrollEvent;
   headlineImage = document.querySelector('.indiv-img__one');
-  headlineImageNarrowScroll = 0;
-  headlineImageWideScroll = 18;
+  // headlineImageNarrowScroll = 0;
+  headlineImageWideScroll = 30;
   //section 2
   scrollEventTwo;
   sectionTwoImageLeft = document.querySelector('.indiv-img__two-left');
   sectionTwoImageRight = document.querySelector('.indiv-img__two-right');
-  sectionTwoNarrowScroll = 0;
+  // sectionTwoNarrowScroll = 0;
   sectionTwoWideScroll = 10;
   //section 3
   scrollEventThree;
@@ -24,8 +26,8 @@ class IndividualView {
   //section 4
   scrollEventFour;
   sectionFourImage = document.querySelector('.indiv-img__four');
-  sectionFourNarrowScroll = -4;
-  // sectionFourWideScroll = 10;
+  // sectionFourNarrowScroll = -4;
+  sectionFourWideScroll = -3;
 
   indivSectionsObserver = new IntersectionObserver(this.runScrollAnimation.bind(this), { root: null, threshold: 0.05 });
   constructor() {
@@ -33,40 +35,70 @@ class IndividualView {
       window.scrollTo(0, 0);
     };
 
+    // this.indivScrolledSection.style.height =
+    //   Array.from(this.allIndivSections)
+    //     .map((section) => section.offsetWidth)
+    //     .reduce((ttl, sectionWidth) => (ttl += sectionWidth), 0) -
+    //   (window.innerWidth - window.innerHeight) -
+    //   16 * 16;
+
+    this.indivScrolledSection.style.height = window.innerWidth * 4 - (window.innerWidth - window.innerHeight);
+
+    this.maxWidthPossible = window.innerWidth * 4 - (window.innerWidth - window.innerHeight) - window.innerWidth;
+
+    window.addEventListener(
+      'resize',
+      function () {
+        console.log('resized');
+        this.maxWidthPossible = window.innerWidth * 4 - (window.innerWidth - window.innerHeight) - window.innerWidth;
+        this.indivScrolledSection.style.height = window.innerWidth * 4 - (window.innerWidth - window.innerHeight);
+      }.bind(this)
+    );
+
+    // this.indivMainSection.style.width =
+    //   Array.from(this.allIndivSections)
+    //     .map((section) => section.offsetWidth)
+    //     .reduce((ttl, sectionWidth) => (ttl += sectionWidth), 0) * 0.88;
+
     this.allIndivSections.forEach((section) => {
       this.indivSectionsObserver.observe(section);
     });
   }
 
   runScrollAnimation(entry, observer) {
+    if (!window.matchMedia('(hover: hover)').matches) {
+      console.log(entry[0].target.id);
+      observer.unobserve(entry[0].target);
+      return;
+    }
     if (entry[0].target.id === '1-indiv') {
-      if (window.innerWidth < 1080) {
-        window.addEventListener('scroll', this.narrActivateIndivOne.bind(this));
-      }
+      // if (window.innerWidth < 1080) {
+      //   window.addEventListener('scroll', this.narrActivateIndivOne.bind(this));
+      // }
       if (window.innerWidth > 1080) {
         window.addEventListener('scroll', this.wideActivateIndivOne.bind(this));
       }
     }
     if (entry[0].target.id === '2-indiv') {
-      if (window.innerWidth < 1080) {
-        window.addEventListener('scroll', this.narrActivateIndivTwo.bind(this));
-      }
+      // if (window.innerWidth < 1080) {
+      //   window.addEventListener('scroll', this.narrActivateIndivTwo.bind(this));
+      // }
       if (window.innerWidth > 1080) {
         window.addEventListener('scroll', this.wideActivateIndivTwo.bind(this));
       }
     }
     if (entry[0].target.id === '3-indiv') {
-      if (window.innerWidth < 1080) {
-        window.addEventListener('scroll', this.narrActivateIndivThree.bind(this));
-      }
+      // if (window.innerWidth < 1080) {
+      //   window.addEventListener('scroll', this.narrActivateIndivThree.bind(this));
+      // }
       if (window.innerWidth > 1080) {
         window.addEventListener('scroll', this.wideActivateIndivThree.bind(this));
       }
     }
     if (entry[0].target.id === '4-indiv') {
-      if (window.innerWidth < 1080) {
-        window.addEventListener('scroll', this.narrActivateIndivFour.bind(this));
-      }
+      // if (window.innerWidth < 1080) {
+      //   window.addEventListener('scroll', this.narrActivateIndivFour.bind(this));
+      // }
       if (window.innerWidth > 1080) {
         window.addEventListener('scroll', this.wideActivateIndivFour.bind(this));
       }
@@ -75,24 +107,25 @@ class IndividualView {
     observer.unobserve(entry[0].target);
   }
 
-  narrActivateIndivOne() {
-    if (this.scrollEvent < window.pageYOffset) {
-      //headline image one
-      this.headlineImageNarrowScroll = this.headlineImageNarrowScroll - (window.pageYOffset - this.scrollEvent) / 45;
-      this.headlineImage.style.transform = `translateY(${this.headlineImageNarrowScroll}rem)`;
-    }
-    if (this.scrollEvent > window.pageYOffset) {
-      //headline image one
-      this.headlineImageNarrowScroll = this.headlineImageNarrowScroll + (this.scrollEvent - window.pageYOffset) / 45;
-      this.headlineImage.style.transform = `translateY(${this.headlineImageNarrowScroll}rem)`;
-    }
-    this.scrollEvent = window.pageYOffset;
-  }
+  // narrActivateIndivOne() {
+  //   if (this.scrollEvent > this.maxWidthPossible) return;
+  //   if (this.scrollEvent < window.pageYOffset) {
+  //     //headline image one
+  //     this.headlineImageNarrowScroll = this.headlineImageNarrowScroll - (window.pageYOffset - this.scrollEvent) / 45;
+  //     this.headlineImage.style.transform = `translateY(${this.headlineImageNarrowScroll}rem)`;
+  //   }
+  //   if (this.scrollEvent > window.pageYOffset) {
+  //     //headline image one
+  //     this.headlineImageNarrowScroll = this.headlineImageNarrowScroll + (this.scrollEvent - window.pageYOffset) / 45;
+  //     this.headlineImage.style.transform = `translateY(${this.headlineImageNarrowScroll}rem)`;
+  //   }
+  //   this.scrollEvent = window.pageYOffset;
+  // }
 
   wideActivateIndivOne() {
     if (this.scrollEvent < window.pageYOffset) {
       if (!this.headerBox.classList.contains('header-scroll-started')) {
-        this.headerBox.style.transition = 'transform 0.3s linear';
+        this.headerBox.style.transition = 'transform 0.3s linear, opacity 0.3s linear';
         this.footerBox.style.transition = 'transform 0.3s linear';
         this.headerBox.classList.add('header-scroll-started');
         this.footerBox.classList.add('footer-scroll-started');
@@ -120,33 +153,34 @@ class IndividualView {
     this.scrollEvent = window.pageYOffset;
   }
 
-  narrActivateIndivTwo() {
-    // console.log(window.pageYOffset - this.scrollEventTwo);
-    if (this.scrollEventTwo < window.pageYOffset) {
-      //headline image one
-      this.sectionTwoNarrowScroll = this.sectionTwoNarrowScroll - (window.pageYOffset - this.scrollEventTwo) / 105;
-      this.sectionTwoImageLeft.style.transform = `translateX(${
-        this.sectionTwoNarrowScroll > 0
-          ? -this.sectionTwoNarrowScroll.toFixed(3)
-          : Math.abs(this.sectionTwoNarrowScroll.toFixed(3))
-      }rem)`;
-      this.sectionTwoImageRight.style.transform = `translateX(${this.sectionTwoNarrowScroll.toFixed(3)}rem)`;
-    }
-    if (this.scrollEventTwo > window.pageYOffset) {
-      //headline image one
-      this.sectionTwoNarrowScroll = this.sectionTwoNarrowScroll + (this.scrollEventTwo - window.pageYOffset) / 105;
-      this.sectionTwoImageLeft.style.transform = `translateX(${
-        this.sectionTwoNarrowScroll > 0
-          ? -this.sectionTwoNarrowScroll.toFixed(3)
-          : Math.abs(this.sectionTwoNarrowScroll.toFixed(3))
-      }rem)`;
-      this.sectionTwoImageRight.style.transform = `translateX(${this.sectionTwoNarrowScroll.toFixed(3)}rem)`;
-    }
+  // narrActivateIndivTwo() {
+  //   // console.log(window.pageYOffset - this.scrollEventTwo);
+  //   if (this.scrollEventTwo < window.pageYOffset) {
+  //     //headline image one
+  //     this.sectionTwoNarrowScroll = this.sectionTwoNarrowScroll - (window.pageYOffset - this.scrollEventTwo) / 105;
+  //     this.sectionTwoImageLeft.style.transform = `translateX(${
+  //       this.sectionTwoNarrowScroll > 0
+  //         ? -this.sectionTwoNarrowScroll.toFixed(3)
+  //         : Math.abs(this.sectionTwoNarrowScroll.toFixed(3))
+  //     }rem)`;
+  //     this.sectionTwoImageRight.style.transform = `translateX(${this.sectionTwoNarrowScroll.toFixed(3)}rem)`;
+  //   }
+  //   if (this.scrollEventTwo > window.pageYOffset) {
+  //     //headline image one
+  //     this.sectionTwoNarrowScroll = this.sectionTwoNarrowScroll + (this.scrollEventTwo - window.pageYOffset) / 105;
+  //     this.sectionTwoImageLeft.style.transform = `translateX(${
+  //       this.sectionTwoNarrowScroll > 0
+  //         ? -this.sectionTwoNarrowScroll.toFixed(3)
+  //         : Math.abs(this.sectionTwoNarrowScroll.toFixed(3))
+  //     }rem)`;
+  //     this.sectionTwoImageRight.style.transform = `translateX(${this.sectionTwoNarrowScroll.toFixed(3)}rem)`;
+  //   }
 
-    // console.log(this.sectionTwoNarrowScroll, (window.pageYOffset - this.scrollEventTwo) / 20);
-    //renew scrollEvent
-    this.scrollEventTwo = window.pageYOffset;
-  }
+  //   // console.log(this.sectionTwoNarrowScroll, (window.pageYOffset - this.scrollEventTwo) / 20);
+  //   //renew scrollEvent
+  //   this.scrollEventTwo = window.pageYOffset;
+  // }
+
   wideActivateIndivTwo() {
     // console.log(window.pageYOffset - this.scrollEventTwo);
     if (this.scrollEventTwo < window.pageYOffset) {
@@ -171,20 +205,20 @@ class IndividualView {
     this.scrollEventTwo = window.pageYOffset;
   }
 
-  narrActivateIndivThree() {
-    if (this.scrollEventThree < window.pageYOffset) {
-      // console.log(this.sectionThreeNarrowScroll);
-      //headline image one
-      this.sectionThreeNarrowScroll = this.sectionThreeNarrowScroll + (window.pageYOffset - this.scrollEventThree) / 65;
-      this.sectionThreeImage.style.transform = `translateX(${this.sectionThreeNarrowScroll}rem) translateY(-${this.sectionThreeNarrowScroll}rem) `;
-    }
-    if (this.scrollEventThree > window.pageYOffset) {
-      //headline image one
-      this.sectionThreeNarrowScroll = this.sectionThreeNarrowScroll - (this.scrollEventThree - window.pageYOffset) / 65;
-      this.sectionThreeImage.style.transform = `translateX(${this.sectionThreeNarrowScroll}rem) translateY(-${this.sectionThreeNarrowScroll}rem)`;
-    }
-    this.scrollEventThree = window.pageYOffset;
-  }
+  // narrActivateIndivThree() {
+  //   if (this.scrollEventThree < window.pageYOffset) {
+  //     // console.log(this.sectionThreeNarrowScroll);
+  //     //headline image one
+  //     this.sectionThreeNarrowScroll = this.sectionThreeNarrowScroll + (window.pageYOffset - this.scrollEventThree) / 65;
+  //     this.sectionThreeImage.style.transform = `translateX(${this.sectionThreeNarrowScroll}rem) translateY(-${this.sectionThreeNarrowScroll}rem) `;
+  //   }
+  //   if (this.scrollEventThree > window.pageYOffset) {
+  //     //headline image one
+  //     this.sectionThreeNarrowScroll = this.sectionThreeNarrowScroll - (this.scrollEventThree - window.pageYOffset) / 65;
+  //     this.sectionThreeImage.style.transform = `translateX(${this.sectionThreeNarrowScroll}rem) translateY(-${this.sectionThreeNarrowScroll}rem)`;
+  //   }
+  //   this.scrollEventThree = window.pageYOffset;
+  // }
   wideActivateIndivThree() {
     if (this.scrollEventThree < window.pageYOffset) {
       //headline image one
@@ -199,21 +233,32 @@ class IndividualView {
     this.scrollEventThree = window.pageYOffset;
   }
 
-  narrActivateIndivFour() {
+  // narrActivateIndivFour() {
+  //   if (this.scrollEventFour < window.pageYOffset) {
+  //     //headline image one
+  //     this.sectionFourNarrowScroll = this.sectionFourNarrowScroll + (window.pageYOffset - this.scrollEventFour) / 60;
+  //     this.sectionFourImage.style.transform = `translateY(${this.sectionFourNarrowScroll}rem)`;
+  //   }
+  //   if (this.scrollEventFour > window.pageYOffset) {
+  //     //headline image one
+  //     this.sectionFourNarrowScroll = this.sectionFourNarrowScroll - (this.scrollEventFour - window.pageYOffset) / 60;
+  //     this.sectionFourImage.style.transform = `translateY(${this.sectionFourNarrowScroll}rem)`;
+  //   }
+  //   this.scrollEventFour = window.pageYOffset;
+  // }
+
+  wideActivateIndivFour() {
     if (this.scrollEventFour < window.pageYOffset) {
       //headline image one
-      this.sectionFourNarrowScroll = this.sectionFourNarrowScroll + (window.pageYOffset - this.scrollEventFour) / 60;
-      this.sectionFourImage.style.transform = `translateY(${this.sectionFourNarrowScroll}rem)`;
+      this.sectionFourWideScroll = this.sectionFourWideScroll + (window.pageYOffset - this.scrollEventFour) / 400;
+      this.sectionFourImage.style.transform = `translateY(${this.sectionFourWideScroll}rem)`;
     }
     if (this.scrollEventFour > window.pageYOffset) {
       //headline image one
-      this.sectionFourNarrowScroll = this.sectionFourNarrowScroll - (this.scrollEventFour - window.pageYOffset) / 60;
-      this.sectionFourImage.style.transform = `translateY(${this.sectionFourNarrowScroll}rem)`;
+      this.sectionFourWideScroll = this.sectionFourWideScroll - (this.scrollEventFour - window.pageYOffset) / 400;
+      this.sectionFourImage.style.transform = `translateY(${this.sectionFourWideScroll}rem)`;
     }
     this.scrollEventFour = window.pageYOffset;
-  }
-  wideActivateIndivFour() {
-    // console.log('wide Activated on div4');
   }
 }
 
